a087d4cc7b9fd1ced12dc03d436433a38e7fdb9a
Ricky Suprayudi <juraganled@gmail.com>
2024-05-07 11:17:27 +0700

    add GaugeView and a slider to change the value

---
+++ PlayingWithSwiftUI67/GaugeView.swift
@@ -0,0 +1,18 @@
+//
+//  GaugeView.swift
+//  PlayingWithSwiftUI67
+//
+//  Created by Ricky Suprayudi on 07/05/24.
+//
+
+import SwiftUI
+
+struct GaugeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GaugeView()
+}
